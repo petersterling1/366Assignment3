874d8a7a20795a6c7649616f8477617cff250823
petersterling1 <petersterling1>
2015-12-02 14:24:24 -0800

    added files

---
+++ ExpectedSarsa.py
@@ -0,0 +1,76 @@
+
+"""
+Written Assignment 1: CMPUT 366
+Written by Peter Sterling, Eric Smith: November 5, 2015 
+"""
+import blackjack
+from pylab import *
+import numpy as n
+
+evaluationEpisodes = 3000000
+learningEpisodes = 3000000
+numEpisodes = evaluationEpisodes + learningEpisodes
+
+states = 0.00001*n.random.rand(181,2)
+epsilonMu = 0.2
+epsilonPi = 0.0
+alpha = 0.0005
+discount = 1
+epsilon = epsilonMu
+
+def returnPolicy(state): return n.argmax(states[state])
+
+returnSum = 0.0
+for episodeNum in range(numEpisodes):
+    G = 0
+    #Start a new game of blackjack
+    currentState = blackjack.init()
+    #Continue this game until the terminal state is reached
+    while(currentState != -1):
+        #Get a random number between 0 and 1, if its less than epsilon behavior, then explore
+       
+        rnumber = n.random.rand()
+        if rnumber < epsilon:
+            action = n.random.randint(2)
+        else:
+	    #If not exploring, pick the highest action at state S
+            action = returnPolicy(currentState)      
+	
+	#Get the next state, get reward and next state
+        next = blackjack.sample(currentState, action)
+        reward = next[0]
+        nextstate = next[1]
+        #Add to return
+        G = G + reward
+        
+        #Get chance of being greedy
+        greedychance = 1-epsilon
+        
+        #Get best value at the next state
+        highest = argmax(states[nextstate])
+        
+        #Expected sarsa calculation (greedy * best_next_state_action) + (explore * (0.5*next_state_action1 + 0.5*next_state_action2))
+        target = (greedychance * states[nextstate][highest]) + (epsilon * (0.5*states[nextstate][0] + 0.5*states[nextstate][1]))
+        states[currentState][action] = states[currentState][action] + alpha * (reward + target - states[currentState][action]) 
+            
+        currentState = nextstate
+            
+	#print "Episode: ", episodeNum, "Return: ", G
+    returnSum = returnSum + G
+    if(episodeNum%10000 == 0 and episodeNum != 0 and episodeNum != learningEpisodes):
+        if episodeNum < learningEpisodes:
+           #Print the running average while learning 
+           print "Average return ",episodeNum,": ", returnSum/episodeNum
+        else:
+           #Print the average of just the episodes used in evaluation mode (non-learning)
+           print "Average return ",episodeNum,": ", returnSum/(episodeNum-learningEpisodes)
+
+    if episodeNum == learningEpisodes: 
+        epsilon = epsilonPi
+        alpha = 0
+        returnSum = 0.0
+        blackjack.printPolicy(returnPolicy)
+    
+print "Average return: ", returnSum/evaluationEpisodes
+#Print the policy
+blackjack.printPolicy(returnPolicy)
---
+++ Tilecoder.py
@@ -0,0 +1,25 @@
+numTilings = 8
+offset = (0.6/8)
+tileWidth = 0.6
+
+#import numpy as np
+
+#        X = x + (i*offset); Y = y + (i*offset)
+#       X = int(x/0.6); Y = int(y/0.6)
+
+def tilecode(x,y,tileIndices):
+    for i in range(0,numTilings):
+        X = int(x/0.6); Y = int(y/0.6)
+        index = ((Y*11)+X)+(121*i)
+        tileIndices[i] = index
+        x=x+offset; y=y+offset
+    
+def printTileCoderIndices(x,y):
+    tileIndices = [-1]*numTilings
+    tilecode(x,y,tileIndices)
+    print 'Tile indices for input (',x,',',y,') are : ', tileIndices
+
+printTileCoderIndices(0.1,0.1)
+printTileCoderIndices(4.0,2.0)
+printTileCoderIndices(5.99,5.99)
+printTileCoderIndices(4.0,2.1)
